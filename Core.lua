e91829b97a6702a087c767ac5a5dae50bb712976
Ola Bråten <ola.braaten@gmail.com>
2019-11-25 23:23:10 +0100

    Initial commit

---
+++ Core.lua
@@ -0,0 +1,180 @@
+local SLOTS = {
+	"HeadSlot",
+	"ShoulderSlot",
+	"BackSlot",
+	"ChestSlot",
+	"ShirtSlot",
+	"TabardSlot",
+	"WristSlot",
+	
+	"HandsSlot",
+	"WaistSlot",
+	"LegsSlot",
+	"FeetSlot",
+	
+	"MainHandSlot",
+	"SecondaryHandSlot",
+}
+
+local HIDDEN_SOURCES = {
+	[77344] = true, -- head
+	[77343] = true, -- shoulder
+	[77345] = true, -- back
+	[83202] = true, -- shirt
+	[83203] = true, -- tabard
+	[84223] = true, -- waist
+}
+
+local buttons = {}
+local undressButton
+
+local updateSlots
+
+-- Toggle buttons visibility
+local function showButtons(show)
+    for slot, button in pairs(buttons) do
+        if show then
+            button:Show()
+        else
+            button:Hide()
+        end
+    end
+    if show then
+        undressButton:Show()
+    else
+        undressButton:Hide()
+    end
+end
+
+-- Button click event
+local function onClick(self, button)
+	if button == "RightButton" then
+		local slotID, slotTexture = GetInventorySlotInfo(self.slot)
+        DressUpFrame.ModelScene:GetPlayerActor():UndressSlot(slotID)
+        updateSlots()
+	elseif self.item and IsModifiedClick() then
+		HandleModifiedItemClick(self.item)
+	end
+end
+
+-- Button hover event
+local function onEnter(self)
+	GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+	if self.item then
+		GameTooltip:SetHyperlink(self.item)
+	else
+		GameTooltip:SetText(self.text or _G[string.upper(self.slot)])
+	end
+end
+
+-- Button size constants
+local buttonSize = 35
+local buttonSizeWithPadding = buttonSize + 5
+local sideInsetLeft = 10
+local sideInsetRight = 12
+local topInset = -80
+
+-- Create item slot buttons
+for i, slot in ipairs(SLOTS) do
+    local button = CreateFrame("Button", nil, DressUpFrame)
+    button:SetFrameStrata("HIGH")
+    button:SetSize(buttonSize, buttonSize)
+    if i <= 7 then
+        button:SetPoint("TOPLEFT", sideInsetLeft, topInset + -buttonSizeWithPadding * (i - 1))
+    else
+        local place = i
+        if i > 11 then
+            place = place + 1
+        end
+        button:SetPoint("TOPRIGHT", -sideInsetRight, topInset + -buttonSizeWithPadding * (place - 8))
+    end
+    button:RegisterForClicks("LeftButtonUp", "RightButtonUp")
+    button:SetMotionScriptsWhileDisabled(true)
+    button:SetScript("OnClick", onClick)
+    button:SetScript("OnEnter", onEnter)
+    button:SetScript("OnLeave", GameTooltip_Hide)
+    button.slot = slot
+
+    button.icon = button:CreateTexture(nil, "BACKGROUND")
+    button.icon:SetSize(buttonSize, buttonSize)
+    button.icon:SetPoint("CENTER")
+    
+    button.highlight = button:CreateTexture()
+    button.highlight:SetSize(buttonSize, buttonSize)
+    button.highlight:SetPoint("CENTER")
+    button.highlight:SetAtlas("bags-glow-white")
+    button.highlight:SetBlendMode("ADD")
+    button:SetHighlightTexture(button.highlight)
+    
+    buttons[slot] = button
+end
+
+-- Undress button
+undressButton = CreateFrame("Button", nil, DressUpFrame, "UIPanelButtonTemplate")
+undressButton:SetSize(80, 22)
+undressButton:SetText("Undress")
+undressButton:SetPoint("BOTTOMLEFT", 6, 4)
+undressButton:SetScript("OnClick", function()
+    DressUpFrame.ModelScene:GetPlayerActor():Undress()
+    updateSlots()
+end)
+
+-- Updates slot buttons content based on PlayerActor
+updateSlots = function()
+    local playerActor = DressUpFrame.ModelScene:GetPlayerActor()
+    for slot, button in pairs(buttons) do
+        local slotID, slotTexture = GetInventorySlotInfo(slot)
+		local sourceID = playerActor:GetSlotTransmogSources(slotID)
+		if sourceID == NO_TRANSMOG_SOURCE_ID or HIDDEN_SOURCES[sourceID] then
+			button.item = nil
+			button.text = nil
+			button.icon:SetTexture(slotTexture)
+			button:Disable()
+		else
+			local categoryID, appearanceID, canEnchant, icon, isCollected, link = C_TransmogCollection.GetAppearanceSourceInfo(sourceID)
+			button.item = link
+			button.text = UNKNOWN
+			button.icon:SetTexture(icon or [[Interface\Icons\INV_Misc_QuestionMark]])
+			button:Enable()
+		end
+    end
+end
+
+-- Hook onto PlayerActor creation in order to hook onto its functions
+local _SetupPlayerForModelScene = SetupPlayerForModelScene
+function SetupPlayerForModelScene(...)
+    showButtons(true)
+    local resultSetupPlayerForModelScene = _SetupPlayerForModelScene(...)
+    local playerActor = DressUpFrame.ModelScene:GetPlayerActor()
+
+    -- Update slots when a gear piece has changed
+    local _TryOn = playerActor.TryOn
+    playerActor.TryOn = function (...)
+        local resultTryOn = _TryOn(...)
+        updateSlots()
+        return resultTryOn
+    end
+
+    -- Update slots when reset button has been pressed
+    local _Dress = playerActor.Dress
+    playerActor.Dress = function (...)
+        local resultDress = _Dress(...)
+        updateSlots()
+        return resultDress
+    end
+    return resultSetupPlayerForModelScene
+end
+
+-- Hide slot buttons for pet preview
+local _DressUpBattlePet = DressUpBattlePet
+function DressUpBattlePet(...)
+    showButtons(false)
+    return _DressUpBattlePet(...)
+end
+
+-- Hide slots for mount preview
+local _DressUpMount = DressUpMount
+function DressUpMount(...)
+    showButtons(false)
+    return _DressUpMount(...)
+end
---
+++ Readme.md
@@ -0,0 +1,7 @@
+# DressingSlots
+Tiny addon for the World of Warcraft gear dressing room.
+
+## Description
+DressingSlots extends the normal World of Warcraft dressing room by overlaying item slots so that you can see what gear pieces you are currently previewing.
+
+Each gear slot can also be undressed by right clicking it, or you can undress completely with the new undress button at the bottom of the dressing room!
