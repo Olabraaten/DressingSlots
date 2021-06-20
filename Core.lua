183c370715d0218bf23dc91589730bb58e6def07
Ola Bråten <ola.braaten@gmail.com>
2021-06-20 14:18:55 +0200

    Handle secondary appearance.

--- Core.lua
+++ Core.lua
@@ -38,35 +38,66 @@ end
 
 local buttons = {}
 local undressButton
+local toggleSheatheButton
 local showSettingsButton
 local settingsDropdown
 local resizeButton
 
 local updateSlots
+local makePrimarySlotButton
+local makeSecondarySlotButton
 
 -- Toggle buttons visibility
 local function showButtons(show)
-    for slot, button in pairs(buttons) do
-        if show then
-            button:Show()
-        else
-            button:Hide()
+    for slot, slotButtons in pairs(buttons) do
+        for i, button in ipairs(slotButtons) do
+            if show then
+                if i == 1 then
+                    button:Show()
+                end
+            else
+                button:Hide()
+            end
         end
     end
     if show then
         undressButton:Show()
         showSettingsButton:Show()
+        toggleSheatheButton:Show()
     else
         undressButton:Hide()
         showSettingsButton:Hide()
+        toggleSheatheButton:Hide()
     end
 end
 
 -- Button click event
 local function onClick(self, button)
 	if button == "RightButton" then
-		local slotID, slotTexture = GetInventorySlotInfo(self.slot)
-        DressUpFrame.ModelScene:GetPlayerActor():UndressSlot(slotID)
+        local playerActor = DressUpFrame.ModelScene:GetPlayerActor()
+        local slotID = GetInventorySlotInfo(self.slot)
+        local itemTransmogInfo = playerActor:GetItemTransmogInfo(slotID)
+        if itemTransmogInfo.secondaryAppearanceID ~= Constants.Transmog.NoTransmogID then
+            itemTransmogInfo.appearanceID = itemTransmogInfo.secondaryAppearanceID
+            itemTransmogInfo.secondaryAppearanceID = Constants.Transmog.NoTransmogID
+            playerActor:SetItemTransmogInfo(itemTransmogInfo, slotID, false)
+        else
+            playerActor:UndressSlot(slotID)
+        end
+        updateSlots()
+        PlaySound(SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON)
+	elseif self.item and IsModifiedClick() then
+		HandleModifiedItemClick(self.item)
+	end
+end
+
+local function secondaryOnClick(self, button)
+	if button == "RightButton" then
+        local playerActor = DressUpFrame.ModelScene:GetPlayerActor()
+        local slotID = GetInventorySlotInfo(self.slot)
+        local itemTransmogInfo = playerActor:GetItemTransmogInfo(slotID)
+        itemTransmogInfo.secondaryAppearanceID = Constants.Transmog.NoTransmogID
+        playerActor:SetItemTransmogInfo(itemTransmogInfo, slotID, false)
         updateSlots()
         PlaySound(SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON)
 	elseif self.item and IsModifiedClick() then
@@ -86,14 +117,16 @@ end
 
 -- Button size constants
 local buttonSize = 35
+local secondaryButtonSize = 25
 local buttonSizeWithPadding = buttonSize + 5
 local sideInsetLeft = 10
 local sideInsetRight = 12
 local topInset = -80
 
 -- Create item slot buttons
-for i, slot in ipairs(SLOTS) do
+makePrimarySlotButton = function(i, slot)
     local button = CreateFrame("Button", nil, DressUpFrame)
+    button.slot = slot
     button:SetFrameStrata("HIGH")
     button:SetSize(buttonSize, buttonSize)
     if i <= 7 then
@@ -110,7 +143,6 @@ for i, slot in ipairs(SLOTS) do
     button:SetScript("OnClick", onClick)
     button:SetScript("OnEnter", onEnter)
     button:SetScript("OnLeave", GameTooltip_Hide)
-    button.slot = slot
 
     button.icon = button:CreateTexture(nil, "BACKGROUND")
     button.icon:SetSize(buttonSize, buttonSize)
@@ -122,10 +154,52 @@ for i, slot in ipairs(SLOTS) do
     button.highlight:SetAtlas("bags-glow-white")
     button.highlight:SetBlendMode("ADD")
     button:SetHighlightTexture(button.highlight)
+
+    return button
+end
+
+makeSecondarySlotButton = function(i, slot)
+    local button = CreateFrame("Button", nil, DressUpFrame)
+    button.slot = slot
+    button:SetFrameStrata("HIGH")
+    button:SetSize(secondaryButtonSize, secondaryButtonSize)
+    if i <= 7 then
+        button:SetPoint("TOPLEFT", sideInsetLeft + buttonSizeWithPadding, topInset + -buttonSizeWithPadding * (i - 1))
+    else
+        local place = i
+        if i > 11 then
+            place = place + 1
+        end
+        button:SetPoint("TOPRIGHT", -sideInsetRight - buttonSizeWithPadding, topInset + -buttonSizeWithPadding * (place - 8))
+    end
+    button:RegisterForClicks("LeftButtonUp", "RightButtonUp")
+    button:SetMotionScriptsWhileDisabled(true)
+    button:SetScript("OnClick", secondaryOnClick)
+    button:SetScript("OnEnter", onEnter)
+    button:SetScript("OnLeave", GameTooltip_Hide)
+
+    button.icon = button:CreateTexture(nil, "BACKGROUND")
+    button.icon:SetSize(secondaryButtonSize, secondaryButtonSize)
+    button.icon:SetPoint("CENTER")
     
-    buttons[slot] = button
+    button.highlight = button:CreateTexture()
+    button.highlight:SetSize(secondaryButtonSize, secondaryButtonSize)
+    button.highlight:SetPoint("CENTER")
+    button.highlight:SetAtlas("bags-glow-white")
+    button.highlight:SetBlendMode("ADD")
+    button:SetHighlightTexture(button.highlight)
+
+    return button
+end
+
+for i, slot in ipairs(SLOTS) do
+    local primaryButton = makePrimarySlotButton(i, slot)
+    local secondaryButton = makeSecondarySlotButton(i, slot)
+    
+    buttons[slot] = {primaryButton, secondaryButton}
     if masqueGroup then
-        masqueGroup:AddButton(button)
+        masqueGroup:AddButton(primaryButton)
+        masqueGroup:AddButton(secondaryButton)
     end
 end
 
@@ -137,6 +211,17 @@ undressButton:SetPoint("BOTTOMLEFT", 7, 4)
 undressButton:SetScript("OnClick", function()
     DressUpFrame.ModelScene:GetPlayerActor():Undress()
     updateSlots()
+    PlaySound(SOUNDKIT.GS_TITLE_OPTION_OK)
+end)
+
+-- Toggle sheathe button
+toggleSheatheButton = CreateFrame("Button", nil, DressUpFrame, "UIPanelButtonTemplate")
+toggleSheatheButton:SetSize(120, 22)
+toggleSheatheButton:SetText("Toggle sheathe")
+toggleSheatheButton:SetPoint("BOTTOMLEFT", 87, 4)
+toggleSheatheButton:SetScript("OnClick", function()
+    local playerActor = DressUpFrame.ModelScene:GetPlayerActor()
+    playerActor:SetSheathed(not playerActor:GetSheathed())
     PlaySound(SOUNDKIT.GS_TITLE_OPTION_OK)
 end)
 
@@ -185,7 +270,7 @@ showSettingsButton:SetSize(27, 27)
 showSettingsButton:SetNormalTexture("Interface/ChatFrame/UI-ChatIcon-ScrollDown-Up")
 showSettingsButton:SetPushedTexture("Interface/ChatFrame/UI-ChatIcon-ScrollDown-Down")
 showSettingsButton:SetHighlightTexture("Interface/Buttons/UI-Common-MouseHilight", "ADD")
-showSettingsButton:SetPoint("BOTTOMLEFT", 85, 1)
+showSettingsButton:SetPoint("BOTTOMLEFT", 207, 1)
 showSettingsButton:SetScript("OnClick", function(self)
     ToggleDropDownMenu(1, nil, settingsDropdown, self, 0, 0)
     PlaySound(SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON)
@@ -212,21 +297,35 @@ end)
 updateSlots = function()
     local playerActor = DressUpFrame.ModelScene:GetPlayerActor()
     if playerActor then
-        for slot, button in pairs(buttons) do
+        for slot, slotButtons in pairs(buttons) do
+            local primaryButton = slotButtons[1]
+            local secondaryButton = slotButtons[2]
             local slotID, slotTexture = GetInventorySlotInfo(slot)
             local itemTransmogInfo = playerActor:GetItemTransmogInfo(slotID)
 		    if itemTransmogInfo == nil or HIDDEN_SOURCES[itemTransmogInfo.appearanceID] then
-			    button.item = nil
-			    button.text = nil
-			    button.icon:SetTexture(slotTexture)
-			    button:Disable()
+			    primaryButton.item = nil
+			    primaryButton.text = nil
+			    primaryButton.icon:SetTexture(slotTexture)
+			    primaryButton:Disable()
 		    else
 			    local categoryID, appearanceID, canEnchant, icon, isCollected, link = C_TransmogCollection.GetAppearanceSourceInfo(itemTransmogInfo.appearanceID)
-			    button.item = link
-			    button.text = UNKNOWN
-			    button.icon:SetTexture(icon or [[Interface\Icons\INV_Misc_QuestionMark]])
-			    button:Enable()
+			    primaryButton.item = link
+			    primaryButton.text = UNKNOWN
+			    primaryButton.icon:SetTexture(icon or [[Interface\Icons\INV_Misc_QuestionMark]])
+			    primaryButton:Enable()
 		    end
+            if itemTransmogInfo ~= nil and itemTransmogInfo.secondaryAppearanceID ~= Constants.Transmog.NoTransmogID and not HIDDEN_SOURCES[itemTransmogInfo.secondaryAppearanceID] then
+                local categoryID, appearanceID, canEnchant, icon, isCollected, link = C_TransmogCollection.GetAppearanceSourceInfo(itemTransmogInfo.secondaryAppearanceID)
+			    secondaryButton.item = link
+			    secondaryButton.text = UNKNOWN
+			    secondaryButton.icon:SetTexture(icon or [[Interface\Icons\INV_Misc_QuestionMark]])
+			    secondaryButton:Enable()
+                if DressUpFrame.ResetButton:IsShown() then
+                    secondaryButton:Show()
+                end
+            else
+                secondaryButton:Hide()
+            end
         end
     end
 end
@@ -270,27 +369,27 @@ function SetupPlayerForModelScene(...)
     local resultSetupPlayerForModelScene = _SetupPlayerForModelScene(...)
     local playerActor = DressUpFrame.ModelScene:GetPlayerActor()
     if playerActor then
-        showButtons(true)
         if DressMode == START_UNDRESSED_MODE then
             DressUpFrame.ModelScene:GetPlayerActor():Undress()
         end
+
+        -- Nasty workaround for when shoulders have been undressed while secondary appearance is active
+        local _GetItemTransmogInfo = playerActor.GetItemTransmogInfo
+        function playerActor:GetItemTransmogInfo(slotId, ...)
+            local result = _GetItemTransmogInfo(self, slotId, ...)
+            if not result and slotId == 3 then
+                result = ItemUtil.CreateItemTransmogInfo(77343)
+            end
+        return result
+        end
     end
+
     return resultSetupPlayerForModelScene
 end
 
--- Hide buttons for pet preview
-local _DressUpBattlePet = DressUpBattlePet
-function DressUpBattlePet(...)
+DressUpFrame.ResetButton:HookScript("OnHide", function ()
     showButtons(false)
-    return _DressUpBattlePet(...)
-end
-
--- Hide buttons for mount preview
-local _DressUpMount = DressUpMount
-function DressUpMount(...)
-    -- Trying to preview a crafting recipe will trigger this code without this check
-    if ... ~= 0 then
-        showButtons(false)
-    end
-    return _DressUpMount(...)
-end
+end)
+DressUpFrame.ResetButton:HookScript("OnShow", function ()
+    showButtons(true)
+end)
